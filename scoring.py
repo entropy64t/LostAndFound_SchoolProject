f0ed43efcf8a51bd4654cda93283304ee144353f
bruno-0001 <107435313+bruno-0001@users.noreply.github.com>
2026-05-31 21:55:11 +0200

    Scoring now respects suspected ownership

--- scoring.py
+++ scoring.py
@@ -17,13 +17,27 @@ def score_single(target: Report, item: Report) -> float:
     if target.category != item.category: return 0
     
     colour_score = 0.1 if target.colour == item.colour else 0.01
+
     loc_score = 0
     target_loc = get_location(target.last_seen_location)
     item_loc = get_location(item.last_seen_location)
-    loc_score = 0.1 if target_loc.building_level == item_loc.building_level else 0
-    loc_score = 0.2 if target_loc == item_loc else 0
-    
-    return colour_score + loc_score
+    if target_loc != None and item_loc != None:
+        if target_loc == item_loc:
+            loc_score = 0.2
+        elif target_loc.building_level == item_loc.building_level:
+            loc_score = 0.1
+
+    ownership_score = 0
+    if target.item_owner:
+        if item.author:
+            if target.item_owner == item.author:
+                ownership_score = 0.3
+    if item.item_owner:
+        if target.author:
+            if item.item_owner == target.author:
+                ownership_score = 0.3
+
+    return colour_score + loc_score + ownership_score
 
 def score_against(target: Report, items: list[Report]) -> dict[Report, float]:
     """Score `items` against `target`. For criteria look at `score_single`"""
